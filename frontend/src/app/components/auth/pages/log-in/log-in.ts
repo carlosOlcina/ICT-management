209116213e9d407ee9a102b1daadcbc0fd26a5bf
carlosOlcina <carlosolcina23@gmail.com>
2026-03-31 22:28:48 +0200

    feat(frontend): add LogInPage component with initial structure and tests

---
+++ frontend/src/app/components/auth/pages/log-in/log-in.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-log-in-page',
+  imports: [],
+  templateUrl: './log-in.html',
+  styleUrl: './log-in.css',
+})
+export class LogInPage {
+
+}
